1f3a56c1baeb032822220bae96d78ce20cf6c307
Zheng Xiong <xiongzhengbupt@gmail.com>
2016-11-15 21:29:27 -0500

    Update through 8.8

--- Java/Ch 08. Recursion and Dynamic Programming/Q8_05_Recursive_Multiply/Tester.java
+++ Java/Ch 08. Recursion and Dynamic Programming/Q8_05_Recursive_Multiply/Tester.java
@@ -7,7 +7,7 @@ public class Tester {
 	 */
 	public static void main(String[] args) {
 		int max = 1000;
-		int counterA = 0, counterB = 0, counterC = 0, counterD = 0;
+		int counterA = 0, counterB = 0, counterC = 0, counterD = 0, counterE = 0;;
 		
 		for (int a = 0; a < max; a++) {
 			for (int b = 0; b < max; b++) {
@@ -15,6 +15,7 @@ public class Tester {
 				int prodB = QuestionB.minProduct(a, b);
 				int prodC = QuestionC.minProduct(a, b);
 				int prodD = QuestionD.minProduct(a, b);
+				int prodE = Answer1.minProduct(a, b);
 				
 				int product = a * b;
 				
@@ -22,13 +23,15 @@ public class Tester {
 				counterB += QuestionB.counter;
 				counterC += QuestionC.counter;
 				counterD += QuestionD.counter;
+                counterE += Answer1.counter;
 
 				QuestionA.counter = 0;
 				QuestionB.counter = 0;
 				QuestionC.counter = 0;
 				QuestionD.counter = 0;
+                Answer1.counter = 0;
 				
-				if (prodA != product || prodB != product || prodC != product || prodD != product) {
+				if (prodA != product || prodB != product || prodC != product || prodD != product || prodE != product) {
 					System.out.println("Failure: " + a + " * " + b + " = " + product + " instead of (" + prodA + ", " + prodB + ", " + prodC + ", " + prodD + ")");
 					break;
 				} 
@@ -40,6 +43,7 @@ public class Tester {
 		System.out.println("B: " + counterB);
 		System.out.println("C: " + counterC);
 		System.out.println("D: " + counterD);
+        System.out.println("E: " + counterE);
 	}
 
 }
